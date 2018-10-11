0dcecef2e38ae3966d14020d1a16aaa1b4aac1f2
Davide <davidesavarro98@gmail.com>
2018-10-11 13:00:37 +0200

    Esercizio finito

--- src/dosclic/util/Main.java
+++ src/dosclic/util/Main.java
@@ -1,5 +1,26 @@
 package dosclic.util;
-
+import jbook.util.*;
 public class Main {
+	static final int ROWS = 5;
+	static final int COLS = 3;
 
+	public static void main(String args[]) {
+		
+		int[][] mtrx = new int[ROWS][COLS];
+		
+		for(int i=0;i<ROWS;i++) {
+			for(int j=0;j<COLS;j++) {
+				mtrx[i][j] = Input.readInt("Inserire l'intero (" + (i+1) + "," + (j+1) +"): "); 	
+			}
+		}
+		printMatrix(mtrx);
+	}
+	public static void printMatrix(int mtrx[][]) {
+		for(int i=0;i<ROWS;i++) {
+			for(int j=0;j<COLS;j++) {
+				System.out.print(mtrx[i][j] + " "); 	
+			}
+			System.out.println("");
+		}		
+	}
 }
