3361017f651e0db7eb1406d6eae08a8d53889b83
Davide <davidesavarro98@gmail.com>
2018-11-23 20:58:04 +0100

    Email test missing and rubrica to do

--- src/test/TestContatto.java
+++ src/test/TestContatto.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
@@ -21,8 +23,26 @@ public class TestContatto {
 	}
 	
 	@Test
-	public void prova() {
-		
+	public void testEquals() {
+		// Controlla se il contatto c'è
+		Contatto confr = new Contatto("Mario","MarioRossi@gmail.com","123456,654321,567890");
+		assertTrue(prova.equals(confr));
+		// Controlla se non da errori dato un contatto nullo
+		confr=null;
+		assertFalse(prova.equals(confr));
+
+		confr= new Contatto("Luca","MarioRossi@gmail.com","123456,654321,567890");;
+		assertFalse(prova.equals(confr));
+	}
+
+	@Test
+	public void testTel() {
+		prova.inserisciNumTel("678901");
+		assertEquals("[123456, 654321, 567890, 678901]",prova.numTel());
+		prova.eliminaNumTel("567890");
+		assertEquals("[123456, 654321, 678901]",prova.numTel());
+		prova.eliminaNumTel("567896");
+		assertEquals("[123456, 654321, 678901]",prova.numTel());
 	}
 	
 	@After
