521be4f2c3b1ef22eb71003d9bc1a32b1249d0aa
Davide <davidesavarro98@gmail.com>
2018-11-29 12:57:33 +0100

    Only get target to do

---
+++ src/dosclic/utils/Giudice.java
@@ -0,0 +1,69 @@
+package dosclic.utils;
+
+import java.util.ArrayList;
+
+public class Giudice {
+	public final static int LUNGHEZZA = 4;
+	public static final ArrayList<Character> CHARS = new ArrayList<Character>();
+	
+	public static void init() {
+		CHARS.add('0');
+		CHARS.add('1');
+		CHARS.add('2');
+		CHARS.add('3');
+		CHARS.add('4');
+		CHARS.add('5');
+		CHARS.add('6');
+		CHARS.add('7');
+		CHARS.add('8');
+		CHARS.add('9');
+	}
+	
+	
+	public static void reset() {
+		CHARS.clear();
+	}
+	
+	private static boolean validChars(String string) {
+		for(int i=0;i<string.length();i++) {
+			if(!CHARS.contains(string.charAt(i))) return false;
+		}
+		return true;
+	}
+	
+	private static boolean validRepeat(String string) {
+		for(int i=0;i<string.length();i++) {
+			for(int j=0;j<string.length();j++) {
+				if(j!=i && string.charAt(i) == string.charAt(j)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static boolean valida(String string) {
+		return ((string.length() == LUNGHEZZA) && validChars(string) && validRepeat(string));
+	}
+
+
+	public static int numBulls(String string, String string2) {
+		int numBulls = 0;
+		for(int i=0;i<string2.length();i++) {
+			if(string.charAt(i) == string2.charAt(i)) numBulls++;
+		}
+		return numBulls;
+	}
+
+
+	public static int numMaggots(String string, String string2) {
+		int numMaggots = 0;
+		for(int i=0;i<string.length();i++) {
+			for(int j = 0;j<string2.length();j++) {
+				if((string.charAt(i)==string2.charAt(j)) && (i!=j)) numMaggots++;
+			}
+		}
+		return numMaggots;
+	}
+
+}
---
+++ src/test/TestGiudice.java
@@ -0,0 +1,79 @@
+package test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import dosclic.utils.Giudice;
+
+public class TestGiudice {
+
+	@Before
+	public void init() {
+		Giudice.init();
+	}
+	
+	@Test
+	public void testLunghezza() {
+		assertTrue(Giudice.valida("1456"));
+		assertFalse(Giudice.valida("e6"));
+		assertFalse(Giudice.valida(""));
+		assertFalse(Giudice.valida("097523"));
+	}
+	
+	@Test
+	public void testCaratteriLunghezza() {
+		assertTrue(Giudice.valida("3617"));     
+		assertFalse(Giudice.valida("i907"));     
+		assertFalse(Giudice.valida("567!"));     
+		assertFalse(Giudice.valida("ab%t"));     
+	}
+	
+	@Test
+	public void testValida() {
+		assertFalse(Giudice.valida("1111"));
+		assertFalse(Giudice.valida("0999"));
+		assertFalse(Giudice.valida("8718"));
+		assertTrue(Giudice.valida("1234"));
+		assertTrue(Giudice.valida("8719"));
+	}
+
+	@Test
+	public void testBulls() {
+		assertTrue(Giudice.numBulls("1234", "5678")==0);
+		assertTrue(Giudice.numBulls("1234", "1876")==1);
+		assertTrue(Giudice.numBulls("1234", "9874")==1);
+		assertTrue(Giudice.numBulls("1234", "9243")==1);
+		assertTrue(Giudice.numBulls("9876", "5573")==1);
+		assertTrue(Giudice.numBulls("1234", "1256")==2);
+		assertTrue(Giudice.numBulls("1234", "1537")==2);
+		assertTrue(Giudice.numBulls("1234","1764")==2);
+		assertTrue(Giudice.numBulls("1234", "8237")==2);
+		assertTrue(Giudice.numBulls("1234", "9834")==2);
+		assertTrue(Giudice.numBulls("9264", "9261")==3);
+		assertTrue(Giudice.numBulls("9264", "9284")==3);
+		assertTrue(Giudice.numBulls("9264", "9764")==3);
+		assertTrue(Giudice.numBulls("9264", "5264")==3);
+		assertTrue(Giudice.numBulls("9264", "9264")==4);
+		assertTrue(Giudice.numBulls("8725", "8725")==4);	
+	}
+	
+	@Test
+	public void testMaggots(){
+		// System.out.println(Giudice.numMaggots("1234", "1234"));
+		assertTrue(Giudice.numMaggots("1234", "1234")==0);
+		assertTrue(Giudice.numMaggots("1234", "4321")==4);
+		assertTrue(Giudice.numMaggots("8234", "3746")==2);
+		assertTrue(Giudice.numMaggots("8234", "9763")==1);
+		assertTrue(Giudice.numMaggots("1234", "2345")==3);
+	}
+
+	@After
+	public void reset() {
+		Giudice.reset();
+	}
+
+}
